6d6905834d520925c3f967aee808109696eacd5f
Kartik <tiwarikartik43@gmail.com>
2026-04-22 07:09:29 +0530

    Added ping route for uptime monitoring

--- app.py
+++ app.py
@@ -45,5 +45,9 @@ def create_app():
 
 app = create_app()
 
+@app.route('/ping')
+def ping():
+    return 'pong', 200
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
